c6d31f963a9eeb65c4b1b2f188dc364d83f0e380
Sebastian Zborowski <145390506+sebastian-zborowski@users.noreply.github.com>
2025-07-30 02:43:14 +0200

    Update [AST2] - PASTE_LINK-1.0.user.js

--- [AST2] - PASTE_LINK-1.0.user.js
+++ [AST2] - PASTE_LINK-1.0.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         [AST2] - PASTE_LINK
-// @version      1.0
+// @version      1.1
 // @description  Automatycznie otwiera AST2 z numerem seryjnym zlecenia z Fixably. Rzoszerzenie działa w kooperacji z: FIXABLY_-_INTERFACE_TWEAKS
 // @author       Sebastian Zborowski
 // @match        https://diagnostics.apple.com/*
+// @updateURL    https://raw.githubusercontent.com/sebastian-zborowski/ast2_-_paste_link/main/%5BAST2%5D%20-%20PASTE_LINK-1.0.user.js
+// @downloadURL  https://raw.githubusercontent.com/sebastian-zborowski/ast2_-_paste_link/main/%5BAST2%5D%20-%20PASTE_LINK-1.0.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -11,6 +13,8 @@
 //Niniejszy skrypt został utworzony metodą Vibecodingu. Nie ingeruje trwale w oryginalne strony internetowe, nie odwołuje się do danych prywatnych ani chronionych przepisami RODO,
 //nie przetwarza danych osobowych, a także nie zmienia podstawowego działania strony. Skrypt dodaje kilka automatyzacji, skrótów oraz modyfikacje wizualne, które mają na celu
 //usprawnienie i ułatwienie korzystania z serwisu.
+
+//Ostatnia aktualizacja: 30.07.2025
 
 (function() {
     'use strict';
